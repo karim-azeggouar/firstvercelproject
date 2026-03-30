08ba2ada638a773b9c73f429b6bd665c124352d5
Azeggouar <k.azeggouar@gmail.com>
2026-03-30 09:42:35 +0100

    first commit creation de index.php

---
+++ api/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>     Welcome Dev 101 : </h1>
+
+    <?php
+    $groupe="dev 101";
+    $plt="vercel";
+
+    echo("<h2>   Premier site de $groupe sur $plt </h2>");
+
+
+     ?>
+    
+
+    
+</body>
+</html>
