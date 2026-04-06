e76bf5524109f2383a651dba178e21341675f26e
Azeggouar <k.azeggouar@gmail.com>
2026-04-06 09:18:54 +0100

    upload err

--- api/show_tmp_images.php
+++ api/show_tmp_images.php
@@ -8,8 +8,9 @@ foreach ($files as $file) {
     $path = "/tmp/" . $file;
 
     $data = base64_encode(@file_get_contents($path));
+    $type = @mime_content_type($path);
 
     echo "<p>$file</p>";
-    echo "<img src='data:image;base64,$data' width='250'><br><br>";
+    echo "<img src='data:$type;base64,$data' width='250'><br><br>";
 }
 ?>
